e2dd7524a984d833e33d79ca485ad7fedca1ea67
faignaghi <faignaghi@gmail.com>
2022-11-02 00:50:14 +0100

    commit

--- quiz/models.py
+++ quiz/models.py
@@ -1,1 +1,19 @@
 from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Category Name')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Quizzes'
+
+
+class Quizz(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Quiz title')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
