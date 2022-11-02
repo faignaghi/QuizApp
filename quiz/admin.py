65ddd1370f1041c99a72299b8ecf978f1b261265
faignaghi <faignaghi@gmail.com>
2022-11-02 01:30:22 +0100

    admin site registered

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,1 +1,12 @@
 from django.contrib import admin
+from .models import (
+    Category,
+    Quiz,
+    Question,
+    Option
+)
+
+admin.site.register(Category)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Option)
