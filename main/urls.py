3787920f192cd24adc06471dc29342e31e570cd0
faignaghi <faignaghi@gmail.com>
2022-11-02 01:59:24 +0100

    admin side update

--- main/urls.py
+++ main/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'nested_admin/', include('nested_admin.urls')),
-
 ]
--- quiz/admin.py
+++ quiz/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+import nested_admin
 from .models import (
     Category,
     Quiz,
@@ -6,7 +7,25 @@ from .models import (
     Option
 )
 
+
+class OptionAdmin(nested_admin.NestedTabularInline):
+    model = Option
+    extra = 5
+
+
+class QuestionAdmin(nested_admin.NestedTabularInline):
+    model = Question
+    inlines = [OptionAdmin]
+    extra = 5
+    max_num = 20
+
+
+class QuizAdmin(nested_admin.NestedModelAdmin):
+    model = Quiz
+    inlines = [QuestionAdmin]
+
+
 admin.site.register(Category)
-admin.site.register(Quiz)
+admin.site.register(Quiz, QuizAdmin)
 admin.site.register(Question)
 admin.site.register(Option)
