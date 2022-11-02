dbc0ab7044cd648fe5e7a0d84ced650d49f68bc8
faignaghi <faignaghi@gmail.com>
2022-11-02 01:02:36 +0100

    models updated

--- quiz/models.py
+++ quiz/models.py
@@ -8,12 +8,31 @@ class Category(models.Model):
         return self.name
 
     class Meta:
-        verbose_name_plural = 'Quizzes'
+        verbose_name_plural = 'Categories'
 
 
-class Quizz(models.Model):
+class Quiz(models.Model):
     title = models.CharField(max_length=50, verbose_name='Quiz title')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name_plural = 'Quizzes'
+
+
+class Question(models.Model):
+
+    SCALE = {
+        ('B', 'Beginner')
+        ('I', 'Intermediate')
+        ('A', 'Advanced')
+    }
+    title = models.TextField()
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    difficulty = models.CharField(max_length=1, choices=SCALE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
